9a9bfe9b4b44c4f5ea93b1ed748f69bad544f0a4
Armin Krauss <armin.krauss@utoronto.ca>
2013-03-03 23:45:27 -0500

    Adding grunt - jslinting by default and coffee shared building using grunt coffee

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
+    jshint: {
+      all: ['Gruntfile.js', 'mobile/js/ck.mobile.view.js', 'mobile/js/ck.mobile.js']
+    },
+    coffee: {
+      compile: {
+        files: {
+          'shared/js/ck.model.js': 'shared/coffee/ck.model.coffee',
+          'shared/js/ck.js': 'shared/coffee/ck.coffee'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib');
+  // grunt.loadNpmTasks('grunt-coffee');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint']);
+  
+
+};
