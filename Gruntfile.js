5ffd922b91cbc6631b20c939ba529a725b2ea944
Armin Krauss <armin.krauss@utoronto.ca>
2013-03-04 22:14:57 -0500

    Taking sass out of standard grunt task - so annoying

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
 
   // Default task(s).
   // grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('default', ['jshint', 'coffee', 'sass']);
+  grunt.registerTask('default', ['jshint', 'coffee']);
   
 
 };
